3777b405bdf82dba4b7056dae4b020f824936717
Unknown <marin.boone@edu.ece.fr>
2018-05-01 09:47:59 +0200

    Ajout Navbar

---
+++ accueil.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8" />
+        <title>Page Home</title>
+        <link rel="stylesheet" href="essai.css" />
+    </head>
+
+    <body>
+        
+        <ul>
+          <li><img src="images/photo_essai.jpg" class="img-circle" alt="Avatar"></li>
+          <li><a class="bouton_actif", href="accueil.php">Accueil</a></li>
+          <li><a href="news.asp">Messagerie</a></li>
+          <li><a href="contact.asp">Amis</a></li>
+          <li><a href="notifications.php">Notifications</a></li>
+          <li style="float:right"><a href="#about">Déconnexion</a></li>
+        </ul>
+        
+        <h1>Page d'accueil du site = Mur avec le Fil d'actualité</h1>
+    </body>
+    
+</html>
---
+++ navbar.php
@@ -0,0 +1,10 @@
+        
+<ul>
+    <li><img src="images/photo_essai.jpg" class="img-circle" alt="Avatar"></li>
+    <li><a class="bouton_actif", href="accueil.php">Accueil</a></li>
+    <li><a href="news.asp">Messagerie</a></li>
+    <li><a href="contact.asp">Amis</a></li>
+    <li><a href="notifications.php">Notifications</a></li>
+    <li style="float:right"><a href="#about">Déconnexion</a></li>
+</ul>
+        
