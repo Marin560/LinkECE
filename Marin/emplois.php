210d37168e9aae9116a4bce0b085d39efd7c3f1b
Unknown <marin.boone@edu.ece.fr>
2018-05-01 12:35:31 +0200

    Maj Navbar

---
+++ Marin/emplois.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8" />
+        <title>Emplois</title>
+        <link rel="stylesheet" href="essai.css" />
+    </head>
+
+    <body>
+        
+        <ul>
+          <li class="dropdown">
+                    <a href="javascript:void(0)"><img src="images/photo_essai.jpg" alt="Avatar" style="clickable">
+                    </a>
+                        <div class="dropdown-content">
+                          <a href="#">Mon Reseau</a>
+                          <a href="#">Mes Photos</a>
+                          <a href="#">Mes informations</a>
+                        </div>
+                </li>
+                <li><a class="bouton", href="accueil.php">Accueil</a></li>
+                <li><a class="bouton" href="messagerie.php" class="dropbtn">Messagerie</a></li>
+                <li><a class="bouton" href="notifications.php">Notifications</a></li>
+                <li><a class="bouton_actif bouton" href="emplois.php">Emplois</a></li>
+                <li style="float:right"><a class="bouton" href="#about">Déconnexion</a></li>
+        </ul>
+        
+        <h1>EMPLOIS</h1>
+    </body>
+    
+</html>
