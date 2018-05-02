c9af3ebf6c9d337048e152d3eb5fe3c2e3fc8970
Unknown <marin.boone@edu.ece.fr>
2018-05-02 19:27:22 +0200

    maj

--- connexion.php
+++ connexion.php
@@ -1,42 +1,42 @@
 <!DOCTYPE html>
 <html>
-<meta charset="utf-8">
-<link rel="stylesheet" type = "text/css" href="connexion.css" media ="all"/>  
-<title>Page de connexion LinkECE</title>
-<head>
-
- <p class="logo"> <img src="ece.jpg" alt = "ECE" height =300 width = 300></p>
- <h1>LinkECE</h1>
- <h2> Connexion </h2>
-
-</head>
-
+        
+    <head>
+        <title>Page de connexion LinkECE</title>
+        <meta charset="utf-8"> 
+        <script type="text/javascript" src="script_connexion.js"></script>
+        
+        <p class="logo"> <img src="ece.jpg" alt = "ECE" height =300 width = 300></p>
+        <link rel="stylesheet" type = "text/css" href="connexion.css" media ="all"/>  
+        
+    </head>
+    
 <body>
+    
+    <h1>LinkECE</h1>
+    <h2> Connexion </h2>
 
-  <form method="post" action ="traitement_connexion.php"  >
-         <!-- Formulaire pour envoyer les données de l'utilisateur - Connexion -->  
-    <input type="radio" name="type_user" value="admin">Administrateur<br>
-    <input type="radio" name="type_user" value="utilisateur">Utilisateur <br>
-    E-mail : 
-    <input type="text" name="email" placeholder="Votre e-mail..">
-    Mot de passe : 
-    <input type="text" name="mdp" placeholder="Votre mot de passe..">
-    <input type="submit" name="Valider" value="Se connecter"> 
-  </form>
+     <form method = "post" action ="traitement_connexion.php" onsubmit="return  checkChampsVides(this)">
+        <input type="radio" name="type_user" id="admin" value="admin" >Administrateur<br>
+        <input type="radio" name="type_user" id="utilisateur" value="utilisateur">Utilisateur <br>
+        E-mail : 
+        <input type="text" name="email" id="email" placeholder="Votre e-mail.." onblur="verifEmail(this)" > 
+        Mot de passe : 
+        <input type="text" name="mdp" id="mdp" placeholder="Votre mot de passe.." onblur="verifMdp(this)">   
+        <input type="submit" name="Valider" value="Se connecter">
+    </form>
     
-    <!-- code javascript pour vérifier que les champs ont été remplis correctement -->
-    <script>
-        alert('Hello world!');
-    </script>
-    
-     <a href="modifier.html"><input type="submit" name="modifier" value="Modifier un utilisateur"></a>
+
+    <a href="modifier.html"><input type="submit" name="modifier" value="Modifier un utilisateur"></a>
 
   <h3> Pas de compte? Inscrivez-vous!</h3>
-  <a href="inscription.html"> <input type="submit" name="inscription" value="S'inscrire"  > </a>
+  <a href="inscription.html"> <input type="submit" name="inscription" value="S'inscrire" > </a>
 
 </body>
 
-
+    
+    
+ 
 <footer>
   <p>
     <div id="footer">Copyright &copy; LinkECE<br /> 
@@ -44,6 +44,7 @@
     </div>
   </p>
 </footer>
+
 
 
 </html>
