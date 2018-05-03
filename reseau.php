03e255554309cf295a79f43e2082c2c49f245371
Diego Bazin <diegbazin@gmail.com>
2018-05-03 15:13:05 +0200

    fo

--- reseau.php
+++ reseau.php
@@ -11,7 +11,7 @@ include 'connexion_bdd.php';
 
 if($db_found){
 	
-	$sql = "SELECT DISTINCT prenom,nom,pp,mail FROM user INNER JOIN amitie WHERE id1='".$id."' ";
+	$sql = "SELECT nom,prenom,pp,mail FROM amitie INNER JOIN user ON user.id=amitie.id2 WHERE `id1` = '".$id."'";
 	$recu = mysqli_query($db_handle, $sql) ;
 	$nom=array();
 	$i=0;
