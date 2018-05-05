8a434fed156bc7d2f0b65fb7fe33766f1e9615ef
Unknown <marin.boone@edu.ece.fr>
2018-05-05 14:35:30 +0200

    m

--- mur.php
+++ mur.php
@@ -90,7 +90,7 @@ else{
 					<?php echo '<p>Vous avez<br>'.$_SESSION['nbamis'].'<br>relations</p> ' ?>
 				</div>
                 <div class="well">
-					<?php echo '<a href="images/CV/.pdf">Cliquez ici pour voir votre CV</a>' //Faut choper le bon nom ?>
+					<?php echo '<a href="images/CV/cv'.$id.'.pdf">Cliquez ici pour voir votre CV</a>' //Faut choper le bon nom ?>
 				</div>
 			</div>
 
