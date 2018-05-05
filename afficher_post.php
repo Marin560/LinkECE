4218a099e5e7e057d8fbc31948dece5c0c4b6004
Diego Bazin <diegbazin@gmail.com>
2018-05-05 13:55:02 +0200

    suppr com

--- afficher_post.php
+++ afficher_post.php
@@ -51,13 +51,7 @@ while ($publiamis = mysqli_fetch_assoc($recu1)) {
                             
 						</div>
 					</div>
-					<div class="col-sm-2">
-					</div>
-					<div class="col-sm-10">
-						<div class="row well">
-							<img src="'.$repertoire.$photo[$i].'" class="img-circle" height="55" width="55" alt="Avatar">
-						</div>
-					</div>
+					
 
 
 
